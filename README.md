1eef2e2dd2c5e81beeb0b123d2b7635b79c68a2b
Emil Ravn <emilravn@outlook.com>
2020-06-24 17:29:04 +0200

    initial setup of UI

---
+++ README.md
@@ -0,0 +1,13 @@
+Simple UI so that the player can play the game by clicking on buttons rather than typing their answer in a prompt.
+
+1. For now, remove the logic that plays exactly five rounds.
+
+2. Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+
+3. Add a div for displaying results and change all of your console.logs into DOM methods.
+
+4. Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+5. You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of the programmer’s life.
+
+6. Don’t forget to go back and add your new version to the original Rock Paper Scissors Project!
---
+++ script.js
@@ -0,0 +1,60 @@
+let choice = ["Rock", "Paper", "Scissors"];
+let computerScore = 0;
+let playerScore = 0;
+
+function ComputerPick() {
+    let computerPick = choice[Math.floor(Math.random() * choice.length)];
+    return computerPick;
+}
+
+function PlayerPick() {
+    let userInput = prompt("Rock, paper or scissors?");
+
+    switch (userInput.toLowerCase()) {
+        case "rock":
+            userInput = choice[0];
+            break;
+        case "paper":
+            userInput = choice[1];
+            break;
+        case "scissors":
+            userInput = choice[2];
+            break;
+        default:
+            alert("Not a valid choice!");
+            userInput = PlayerPick();
+    }
+    return userInput;
+}
+
+function lossAlert() {
+    let lossAlert = `You lose! ${PlayerPick} is beaten by ${ComputerPick}.`;
+    computerScore++;
+}
+
+function PlayRound(PlayerPick, ComputerPick) {
+    if (ComputerPick === choice[0] && PlayerPick === choice[2]) {
+        lossAlert();
+    } else if (ComputerPick === choice[2] && PlayerPick === choice[1]) {
+        lossAlert();
+    } else if (ComputerPick === choice[1] && PlayerPick === choice[0]) {
+        lossAlert();
+    } else if (ComputerPick === PlayerPick) {
+        alert(
+            `That's a draw! Computer picked ${ComputerPick} and you picked ${PlayerPick}.`
+        );
+    } else {
+        alert("You win! " + PlayerPick + " beats " + ComputerPick + ".");
+        playerScore++;
+    }
+}
+
+function Game() {
+    alert("Welcome to this game! Here you will play rock, paper, scissors against the computer for five rounds. Good luck!"
+    );
+    for (let i = 0; i < 5; i++) {
+        PlayRound(PlayerPick(), ComputerPick());
+        console.log(`Player's Score: ${playerScore}, Computer's Score: ${computerScore}`
+        );
+    }
+}
