e7de455e9dadc9573485dd25941ae63fc514bcf9
Emil Ravn <emilravn@outlook.com>
2020-07-01 01:36:42 +0200

    fixed that pesky little bug and updated readme.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-A game of RPS made in association with The Odin Project. 
+A classic game of RPS made in association with The Odin Project. It can be be played at [GitHub Pages](https://emilravn.github.io/rockpaperscissors/).
 
-### TODO
-- Add icons as buttons instead of text.
-- Visual indication of player and computer choice with a "rock, paper, scissors" saying.
-- Scaling and mobile optimization.
+## Rules of Rock Paper Scissors
+
+Each turn consists of the player making a distinct choice and the computer making a random choice of either "rock", "paper" or "scissors". The first to reach 5 points win the game. A draw does not equal to a point.
+
+- Rock beats Scissors.
+- Paper beats Rock.
+- Scissors beats Paper.
+
+Have fun!
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let choice = ["Rock", "Paper", "Scissors"];
 let computerScore = 0;
 let playerScore = 0;
 
-// bUttons for the player options
+// Buttons for the player options
 const rockBtn = document.querySelector("#rockbtn");
 const paperBtn = document.querySelector("#paperbtn");
 const scissorsBtn = document.querySelector("#scissorsbtn");
@@ -36,12 +36,12 @@ const updateScore = () => {
     if (playerScore > computerScore) {
       document.getElementById(
         "scoreboard"
-      ).textContent = `You win the game! ${playerChoice} beats ${ComputerPick()}.`;
+      ).textContent = `You win the game! ${playerChoice} beats ${computerChoice}.`;
       RemoveOptions();
     } else {
       document.getElementById(
         "scoreboard"
-      ).textContent = `The computer wins the game! ${playerChoice} is beaten by ${ComputerPick()}.`;
+      ).textContent = `The computer wins the game! ${playerChoice} is beaten by ${computerChoice}.`;
       RemoveOptions();
     }
   }
@@ -56,7 +56,7 @@ function ComputerPick() {
 function lossAlert() {
   document.getElementById(
     "scoreboard"
-  ).textContent = `You lose! ${playerChoice} is beaten by ${ComputerPick()}.`;
+  ).textContent = `You lose! ${playerChoice} is beaten by ${computerChoice}.`;
   computerScore++;
 }
 
@@ -66,7 +66,7 @@ function Versus() {
 }
 
 function PlayRound(playerChoice) {
-  let computerChoice = ComputerPick();
+  window.computerChoice = ComputerPick();
   if (computerChoice === choice[0] && playerChoice === choice[2]) {
     lossAlert();
   } else if (computerChoice === choice[1] && playerChoice === choice[0]) {
